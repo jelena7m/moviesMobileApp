a8cb997b3ced61619ef7659e2b844e520c19c84c
jelena7m <j.mavrak@vegait.rs>
2022-05-31 14:53:35 +0200

    Add initial react native app

---
+++ moviesApp/App.tsx
@@ -0,0 +1,23 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+
+const App = () => {
+  return <View style={styles.container}></View>;
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'black',
+  },
+});
+
+export default App;
